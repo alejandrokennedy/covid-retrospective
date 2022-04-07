afe454f25f908911b7b1ffb8990731de6c6fabc1
Alex Kennedy <akennedy@meredith.com>
2022-04-07 13:57:10 -0400

    test 5

--- main.js
+++ main.js
@@ -459,7 +459,7 @@ async function getData() {
   // const jhuRaw = await d3.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv')
 
   const jhuRaw = await d3.csv('./data/time_series_covid19_confirmed_US.csv')
-  // const us = await d3.json('./data/us.json')
+  const us = await d3.json('./data/us.json')
   // const maxDailyCasesCountiesObj = await d3.json('./data/maxDailyCasesCountiesObj.json')
 
   // const jhuMiddle = performance.now()
@@ -496,7 +496,7 @@ async function getData() {
     }
   });
 
-  d3.selectAll('.step').text('test 4')
+  d3.selectAll('.step').text('test 5')
 
   // console.log('commented out Promise.all')
 
